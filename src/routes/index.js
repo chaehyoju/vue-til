60bbe07497d5eb3fbc40fb77791ee47b874001d9
chaehyoju <agleor@naver.com>
2022-09-13 14:59:06 +0900

    redirect 설정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,23 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+// import LoginPage from '@/views/LoginPage';
+// import SignupPage from '@/views/SignupPage';
 
 Vue.use(VueRouter);
 
-export default new VueRouter();
+export default new VueRouter({
+	routes: [
+		{
+			path: '/',
+			redirect: '/login',
+		},
+		{
+			path: '/login',
+			component: () => import('@/views/LoginPage.vue'),
+		},
+		{
+			path: '/signup',
+			component: () => import('@/views/SignupPage.vue'),
+		},
+	],
+});
